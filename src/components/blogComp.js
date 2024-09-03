458fd36b83e9cd7c5a49958cb062af363bfa393a
Taruna Wahyudi <wahyuditaruna97@gmail.com>
2024-09-04 01:10:27 +0700

    update portfolio 04/09/2024 (2)

--- src/components/blogComp.js
+++ src/components/blogComp.js
@@ -4,15 +4,14 @@ function createSection(data) {
   return `
     <div class="col-md-4 mb-4">
       <div class="card">
-          <img src="${data.urlToImage}" class="card-img-top" alt="Article Image">
+          <img src="${data.thumbnail}" class="card-img-top" alt="Article Image">
           <div class="card-body">
               <h5 class="card-title">${data.title}</h5>
-              <h6 class="card-subtitle mb-2 text-muted">By ${data.author}</h6>
               <p class="card-text">${data.description}</p>
-              <a href="${data.url}" class="btn btn-primary" target="_blank">Read More</a>
+              <a href="${data.link}" class="btn btn-primary" target="_blank">Read More</a>
           </div>
           <div class="card-footer text-muted">
-              Published on: ${new Date(data.publishedAt).toLocaleDateString()}
+              Published on: ${new Date(data.pubDate).toLocaleDateString()}
           </div>
       </div>
     </div>
--- src/pages/BlogPage.js
+++ src/pages/BlogPage.js
@@ -2,13 +2,16 @@ import {addSection} from "../components/blogComp";
 
 export async function load() {
   try {
-    const response = await fetch('https://newsapi.org/v2/everything?q=developer&apiKey=4f4e982eef06402e80e34e880be0d7b5')
-    const responseJson = await response.json();
-    const articles = responseJson.articles;
 
-    const filteredArticles = articles.filter(article => article.urlToImage);
-    filteredArticles.forEach(article => {
-      addSection(article, 'blog_page');
+    const category = 'tekno';
+    const response = await fetch(`https://api-berita-indonesia.vercel.app/antara/${category}`)
+    const responseJson = await response.json();
+
+    const posts = responseJson.data.posts;
+    console.log(posts)
+
+    posts.forEach(post => {
+      addSection(post, 'blog_page');
     })
   } catch (e) {
     console.error(`there was an error: ${e}`);
